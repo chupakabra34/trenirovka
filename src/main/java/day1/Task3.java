69bf4ebc46157990ddef41ff516b841847570928
Sergei Prekrasnov <sap685@gmail.com>
2023-02-13 21:39:16 +0300

    day-1

--- src/main/java/day1/Task3.java
+++ src/main/java/day1/Task3.java
@@ -2,6 +2,8 @@ package day1;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        for (int x = 0; x < 10; x++) {
+            System.out.println("JAVA ");
+        }
     }
 }
--- src/main/java/day1/Task5.java
+++ src/main/java/day1/Task5.java
@@ -2,6 +2,6 @@ package day1;
 
 public class Task5 {
     public static void main(String[] args) {
-
+        for (int year = 1980; year < 2021; year += 4) System.out.printf("Олимпиада %d года \n", year);
     }
 }
